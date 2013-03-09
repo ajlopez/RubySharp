d8738ab5d86687499cd1c5e646d9532042e2f39e
ajlopez <webmaster@ajlopez.com>
2013-03-09 20:11:22 -0300

    ExpressionCommand first test

---
+++ Src/RubySharp.Core.Tests/Commands/ExpressionCommandTests.cs
@@ -0,0 +1,22 @@
+﻿namespace RubySharp.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Commands;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class ExpressionCommandTests
+    {
+        [TestMethod]
+        public void ExecuteConstantExpression()
+        {
+            ExpressionCommand cmd = new ExpressionCommand(new ConstantExpression(1));
+
+            Assert.AreEqual(1, cmd.Execute(null));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Commands/ExpressionCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace RubySharp.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Expressions;
+
+    public class ExpressionCommand : ICommand
+    {
+        private IExpression expression;
+
+        public ExpressionCommand(IExpression expression)
+        {
+            this.expression = expression;
+        }
+
+        public object Execute(Context context)
+        {
+            return this.expression.Evaluate(context);
+        }
+    }
+}
