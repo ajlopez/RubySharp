0b517bf4a0473941fab869472203cc6b68f5f724
ajlopez <webmaster@ajlopez.com>
2013-03-09 17:39:34 -0300

    Context get undefined value as null

---
+++ Src/RubySharp.Core.Tests/ContextTests.cs
@@ -0,0 +1,20 @@
+﻿namespace RubySharp.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ContextTests
+    {
+        [TestMethod]
+        public void GetUndefinedValueAsNull()
+        {
+            Context context = new Context();
+
+            Assert.IsNull(context.GetValue("foo"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Context.cs
@@ -0,0 +1,15 @@
+﻿namespace RubySharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Context
+    {
+        public object GetValue(string name)
+        {
+            return null;
+        }
+    }
+}
