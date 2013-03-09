07847a5a34e3ed662c0c9514f584961999610793
ajlopez <webmaster@ajlopez.com>
2013-03-09 16:51:52 -0300

    Lexer get integer with spaces

--- Src/RubySharp.Core.Tests/Compiler/LexerTests.cs
+++ Src/RubySharp.Core.Tests/Compiler/LexerTests.cs
@@ -48,5 +48,18 @@
 
             Assert.IsNull(lexer.NextToken());
         }
+
+        [TestMethod]
+        public void GetIntegerWithSpaces()
+        {
+            Lexer lexer = new Lexer("  123   ");
+            var result = lexer.NextToken();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("123", result.Value);
+            Assert.AreEqual(TokenType.Integer, result.Type);
+
+            Assert.IsNull(lexer.NextToken());
+        }
     }
 }
