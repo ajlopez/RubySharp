227c482c34bc6a87b3c63f798d035c6fb40abdaf
ajlopez <webmaster@ajlopez.com>
2013-03-09 18:53:13 -0300

    AssignCommand set value

---
+++ Src/RubySharp.Core.Tests/Commands/AssignCommandTests.cs
@@ -0,0 +1,25 @@
+﻿namespace RubySharp.Core.Tests.Commands
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Expressions;
+    using RubySharp.Core.Commands;
+
+    [TestClass]
+    public class AssignCommandTests
+    {
+        [TestMethod]
+        public void AssignValue()
+        {
+            AssignCommand cmd = new AssignCommand("one", new ConstantExpression(1));
+            Context context = new Context();
+
+            cmd.Execute(context);
+
+            Assert.AreEqual(1, context.GetValue("one"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Commands/AssignCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace RubySharp.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Expressions;
+
+    public class AssignCommand
+    {
+        private string name;
+        private IExpression expression;
+
+        public AssignCommand(string name, IExpression expression)
+        {
+            this.name = name;
+            this.expression = expression;
+        }
+
+        public void Execute(Context context)
+        {
+            context.SetValue(this.name, this.expression.Evaluate(context));
+        }
+    }
+}
