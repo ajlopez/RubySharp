092712e2fef985e0cecb4851ba597064ab62f816
ajlopez <webmaster@ajlopez.com>
2013-03-09 18:26:27 -0300

    Refactor, AddExpression, SubtractExpression as BinaryExpression

---
+++ Src/RubySharp.Core/Expressions/BinaryExpression.cs
@@ -0,0 +1,33 @@
+﻿namespace RubySharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public abstract class BinaryExpression : IExpression
+    {
+        private IExpression left;
+        private IExpression right;
+
+        public BinaryExpression(IExpression left, IExpression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public IExpression LeftExpression { get { return this.left; } }
+
+        public IExpression RightExpression { get { return this.right; } }
+
+        public object Evaluate(Context context)
+        {
+            var lvalue = this.left.Evaluate(context);
+            var rvalue = this.right.Evaluate(context);
+
+            return this.Apply(lvalue, rvalue);
+        }
+
+        public abstract object Apply(object leftvalue, object rightvalue);
+    }
+}
--- Src/RubySharp.Core/Expressions/SubtractExpression.cs
+++ Src/RubySharp.Core/Expressions/SubtractExpression.cs
@@ -5,27 +5,16 @@
     using System.Linq;
     using System.Text;
 
-    public class SubtractExpression : IExpression
+    public class SubtractExpression : BinaryExpression
     {
-        private IExpression left;
-        private IExpression right;
-
         public SubtractExpression(IExpression left, IExpression right)
+            : base(left, right)
         {
-            this.left = left;
-            this.right = right;
         }
 
-        public IExpression LeftExpression { get { return this.left; } }
-
-        public IExpression RightExpression { get { return this.right; } }
-
-        public object Evaluate(Context context)
+        public override object Apply(object leftvalue, object rightvalue)
         {
-            int lvalue = (int)this.left.Evaluate(context);
-            int rvalue = (int)this.right.Evaluate(context);
-
-            return lvalue - rvalue;
+            return (int)leftvalue - (int)rightvalue;
         }
     }
 }
