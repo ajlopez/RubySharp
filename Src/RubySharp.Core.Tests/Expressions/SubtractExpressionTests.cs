e4610361c7bd238938026c78b3fba3df9a316dc6
ajlopez <webmaster@ajlopez.com>
2013-03-09 18:21:49 -0300

    SubtractExpression subtract two integers

---
+++ Src/RubySharp.Core.Tests/Expressions/SubtractExpressionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace RubySharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class SubtractExpressionTests
+    {
+        [TestMethod]
+        public void SubtractTwoIntegers()
+        {
+            SubtractExpression expr = new SubtractExpression(new ConstantExpression(2), new ConstantExpression(1));
+            Assert.IsNotNull(expr.LeftExpression);
+            Assert.IsNotNull(expr.RightExpression);
+            Assert.AreEqual(1, expr.Evaluate(null));
+        }
+    }
+}
