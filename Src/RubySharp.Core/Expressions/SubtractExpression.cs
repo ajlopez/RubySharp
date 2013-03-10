26b81cd855138d462aa1a258635dd3c4112bc654
ajlopez <webmaster@ajlopez.com>
2013-03-10 07:57:49 -0300

    SubtractExpression equals

--- Src/RubySharp.Core/Expressions/SubtractExpression.cs
+++ Src/RubySharp.Core/Expressions/SubtractExpression.cs
@@ -16,5 +16,25 @@
         {
             return (int)leftvalue - (int)rightvalue;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is SubtractExpression)
+            {
+                var expr = (SubtractExpression)obj;
+
+                return this.LeftExpression.Equals(expr.LeftExpression) && this.RightExpression.Equals(expr.RightExpression);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.LeftExpression.GetHashCode() + this.RightExpression.GetHashCode() + 17;
+        }
     }
 }
