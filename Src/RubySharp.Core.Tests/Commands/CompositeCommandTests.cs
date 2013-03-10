48bc86b0db6d62835632ed05387d356cb55d298f
ajlopez <webmaster@ajlopez.com>
2013-03-10 08:32:43 -0300

    CompositeCommand equals

--- Src/RubySharp.Core.Tests/Commands/CompositeCommandTests.cs
+++ Src/RubySharp.Core.Tests/Commands/CompositeCommandTests.cs
@@ -25,5 +25,28 @@
             Assert.AreEqual(1, context.GetValue("one"));
             Assert.AreEqual(2, context.GetValue("two"));
         }
+
+        [TestMethod]
+        public void Equals()
+        {
+            AssignCommand acmd1 = new AssignCommand("one", new ConstantExpression(1));
+            AssignCommand acmd2 = new AssignCommand("two", new ConstantExpression(2));
+
+            CompositeCommand cmd1 = new CompositeCommand(new ICommand[] { acmd1, acmd2 });
+            CompositeCommand cmd2 = new CompositeCommand(new ICommand[] { acmd2, acmd1 });
+            CompositeCommand cmd3 = new CompositeCommand(new ICommand[] { acmd1 });
+            CompositeCommand cmd4 = new CompositeCommand(new ICommand[] { acmd1, acmd2 });
+
+            Assert.IsTrue(cmd1.Equals(cmd4));
+            Assert.IsTrue(cmd4.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd4.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(123));
+            Assert.IsFalse(cmd1.Equals(cmd2));
+            Assert.IsFalse(cmd2.Equals(cmd1));
+            Assert.IsFalse(cmd1.Equals(cmd3));
+            Assert.IsFalse(cmd3.Equals(cmd1));
+        }
     }
 }
--- Src/RubySharp.Core/Commands/CompositeCommand.cs
+++ Src/RubySharp.Core/Commands/CompositeCommand.cs
@@ -23,5 +23,40 @@
 
             return result;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is CompositeCommand)
+            {
+                var cmd = (CompositeCommand)obj;
+
+                if (this.commands.Count != cmd.commands.Count)
+                    return false;
+
+                for (int k = 0; k < this.commands.Count; k++)
+                    if (!this.commands[k].Equals(cmd.commands[k]))
+                        return false;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int result = 0;
+
+            foreach (var command in this.commands)
+            {
+                result *= 17;
+                result += command.GetHashCode();
+            }
+
+            return result;
+        }
     }
 }
