2e81abeeca49fa7453892c66ee053f28cd436fb2
ajlopez <webmaster@ajlopez.com>
2013-03-10 07:33:31 -0300

    CompositeCommand execute two assign commands

---
+++ Src/RubySharp.Core.Tests/Commands/CompositeCommandTests.cs
@@ -0,0 +1,29 @@
+﻿namespace RubySharp.Core.Tests.Commands
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Commands;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class CompositeCommandTests
+    {
+        [TestMethod]
+        public void ExecuteTwoAssignCommands()
+        {
+            Context context = new Context();
+            AssignCommand cmd1 = new AssignCommand("one", new ConstantExpression(1));
+            AssignCommand cmd2 = new AssignCommand("two", new ConstantExpression(2));
+            CompositeCommand cmd = new CompositeCommand(new ICommand[] { cmd1, cmd2 });
+
+            var result = cmd.Execute(context);
+
+            Assert.AreEqual(2, result);
+            Assert.AreEqual(1, context.GetValue("one"));
+            Assert.AreEqual(2, context.GetValue("two"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Commands/CompositeCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace RubySharp.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class CompositeCommand : ICommand
+    {
+        private IList<ICommand> commands;
+
+        public CompositeCommand(IList<ICommand> commands)
+        {
+            this.commands = commands;
+        }
+
+        public object Execute(Context context)
+        {
+            object result = null;
+
+            foreach (var command in commands)
+                result = command.Execute(context);
+
+            return result;
+        }
+    }
+}
