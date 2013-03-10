ec01fe0dde86d2a6d2164cef26baed5f6576b16a
ajlopez <webmaster@ajlopez.com>
2013-03-10 07:56:07 -0300

    AddExpression equals

--- Src/RubySharp.Core.Tests/Expressions/AddExpressionTests.cs
+++ Src/RubySharp.Core.Tests/Expressions/AddExpressionTests.cs
@@ -17,5 +17,22 @@
 
             Assert.AreEqual(3, expr.Evaluate(null));
         }
+
+        [TestMethod]
+        public void Equals()
+        {
+            AddExpression expr1 = new AddExpression(new ConstantExpression(1), new ConstantExpression(2));
+            AddExpression expr2 = new AddExpression(new ConstantExpression(1), new ConstantExpression(3));
+            AddExpression expr3 = new AddExpression(new ConstantExpression(1), new ConstantExpression(2));
+
+            Assert.IsTrue(expr1.Equals(expr3));
+            Assert.IsTrue(expr3.Equals(expr1));
+            Assert.AreEqual(expr1.GetHashCode(), expr3.GetHashCode());
+
+            Assert.IsFalse(expr1.Equals(null));
+            Assert.IsFalse(expr1.Equals("foo"));
+            Assert.IsFalse(expr1.Equals(expr2));
+            Assert.IsFalse(expr2.Equals(expr1));
+        }
     }
 }
