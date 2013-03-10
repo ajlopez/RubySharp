48f5db106d90cb7328b9fdafb580e61c47c009eb
ajlopez <webmaster@ajlopez.com>
2013-03-10 08:56:53 -0300

    Machine root context and execute text

---
+++ Src/RubySharp.Core.Tests/MachineTests.cs
@@ -0,0 +1,28 @@
+﻿namespace RubySharp.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MachineTests
+    {
+        [TestMethod]
+        public void HasRootContext()
+        {
+            Machine machine = new Machine();
+            Assert.IsNotNull(machine.RootContext);
+        }
+
+        [TestMethod]
+        public void ExecuteText()
+        {
+            Machine machine = new Machine();
+            Assert.AreEqual(2, machine.ExecuteText("a=1\nb=2"));
+            Assert.AreEqual(1, machine.RootContext.GetValue("a"));
+            Assert.AreEqual(2, machine.RootContext.GetValue("b"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Machine.cs
@@ -0,0 +1,26 @@
+﻿namespace RubySharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Compiler;
+
+    public class Machine
+    {
+        private Context rootcontext = new Context();
+
+        public Context RootContext { get { return this.rootcontext; } }
+
+        public object ExecuteText(string text)
+        {
+            Parser parser = new Parser(text);
+            object result = null;
+
+            for (var command = parser.ParseCommand(); command != null; command = parser.ParseCommand())
+                result = command.Execute(this.rootcontext);
+
+            return result;
+        }
+    }
+}
