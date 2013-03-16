16dc4bf704792f81ff8aaf00f9ff72698a8bdf1a
ajlopez <webmaster@ajlopez.com>
2013-03-16 17:18:03 -0300

    MultiplyExpression with first tests

---
+++ Src/RubySharp.Core/Expressions/MultiplyExpression.cs
@@ -0,0 +1,40 @@
+﻿namespace RubySharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MultiplyExpression : BinaryExpression
+    {
+        public MultiplyExpression(IExpression left, IExpression right)
+            : base(left, right)
+        {
+        }
+
+        public override object Apply(object leftvalue, object rightvalue)
+        {
+            return (int)leftvalue * (int)rightvalue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is MultiplyExpression)
+            {
+                var expr = (MultiplyExpression)obj;
+
+                return this.LeftExpression.Equals(expr.LeftExpression) && this.RightExpression.Equals(expr.RightExpression);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.LeftExpression.GetHashCode() + this.RightExpression.GetHashCode() + typeof(MultiplyExpression).GetHashCode();
+        }
+    }
+}
