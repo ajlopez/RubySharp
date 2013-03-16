9df7bf0202c1b027195f34f0160de55f8d9f0b5b
ajlopez <webmaster@ajlopez.com>
2013-03-16 19:00:30 -0300

    ClassCommand first tests

---
+++ Src/RubySharp.Core.Tests/Commands/ClassCommandTests.cs
@@ -0,0 +1,52 @@
+﻿namespace RubySharp.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Commands;
+    using RubySharp.Core.Expressions;
+    using RubySharp.Core.Functions;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class ClassCommandTests
+    {
+        [TestMethod]
+        public void DefineSimpleClass()
+        {
+            Context context = new Context();
+            ClassCommand cmd = new ClassCommand("Dog", new ExpressionCommand(new CallExpression("puts", new IExpression[] { new ConstantExpression(123) })));
+
+            var result = cmd.Execute(context);
+
+            Assert.IsNull(result);
+
+            var value = context.GetValue("Dog");
+            Assert.IsInstanceOfType(value, typeof(DefinedClass));
+            Assert.AreEqual(value, context.GetValue("Dog"));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            ClassCommand cmd1 = new ClassCommand("foo", new ExpressionCommand(new ConstantExpression(1)));
+            ClassCommand cmd2 = new ClassCommand("bar", new ExpressionCommand(new ConstantExpression(1)));
+            ClassCommand cmd3 = new ClassCommand("foo", new ExpressionCommand(new ConstantExpression(2)));
+            ClassCommand cmd4 = new ClassCommand("foo", new ExpressionCommand(new ConstantExpression(1)));
+
+            Assert.IsTrue(cmd1.Equals(cmd4));
+            Assert.IsTrue(cmd4.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd4.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(123));
+
+            Assert.IsFalse(cmd1.Equals(cmd2));
+            Assert.IsFalse(cmd2.Equals(cmd1));
+            Assert.IsFalse(cmd1.Equals(cmd3));
+            Assert.IsFalse(cmd3.Equals(cmd1));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Commands/ClassCommand.cs
@@ -0,0 +1,49 @@
+﻿namespace RubySharp.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Functions;
+    using RubySharp.Core.Language;
+
+    public class ClassCommand : ICommand
+    {
+        private static int hashcode = typeof(ClassCommand).GetHashCode();
+        private string name;
+        private ICommand command;
+
+        public ClassCommand(string name, ICommand command)
+        {
+            this.name = name;
+            this.command = command;
+        }
+
+        public object Execute(Context context)
+        {
+            var result = new DefinedClass(this.name);
+            context.SetValue(this.name, result);
+            return null;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is ClassCommand)
+            {
+                var cmd = (ClassCommand)obj;
+
+                return this.name == cmd.name && this.command.Equals(cmd.command);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.name.GetHashCode() + this.command.GetHashCode() + hashcode;
+        }
+    }
+}
