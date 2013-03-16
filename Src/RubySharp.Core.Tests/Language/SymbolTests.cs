3a0ad7f76dc8eba1c6242e905f92722dea9899bc
ajlopez <webmaster@ajlopez.com>
2013-03-16 16:10:14 -0300

    Symbol first tests

---
+++ Src/RubySharp.Core.Tests/Language/SymbolTests.cs
@@ -0,0 +1,38 @@
+﻿namespace RubySharp.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class SymbolTests
+    {
+        [TestMethod]
+        public void CreateSymbol()
+        {
+            Symbol symbol = new Symbol("foo");
+
+            Assert.AreEqual(":foo", symbol.ToString());
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            Symbol symbol1 = new Symbol("foo");
+            Symbol symbol2 = new Symbol("bar");
+            Symbol symbol3 = new Symbol("foo");
+
+            Assert.IsTrue(symbol1.Equals(symbol3));
+            Assert.IsTrue(symbol3.Equals(symbol1));
+            Assert.AreEqual(symbol1.GetHashCode(), symbol3.GetHashCode());
+
+            Assert.IsFalse(symbol1.Equals(symbol2));
+            Assert.IsFalse(symbol2.Equals(symbol1));
+            Assert.IsFalse(symbol1.Equals(null));
+            Assert.IsFalse(symbol1.Equals("foo"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Language/Symbol.cs
@@ -0,0 +1,42 @@
+﻿namespace RubySharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Symbol
+    {
+        private string name;
+
+        public Symbol(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return ":" + this.name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is Symbol)
+            {
+                var symbol = (Symbol)obj;
+
+                return this.name == symbol.name;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.name.GetHashCode() + 17;
+        }
+    }
+}
