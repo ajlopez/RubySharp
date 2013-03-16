24fc80dac8611b9e93a22d5aa7ec523157dce307
ajlopez <webmaster@ajlopez.com>
2013-03-16 18:52:35 -0300

    DefinedClass first test

---
+++ Src/RubySharp.Core.Tests/Language/DefinedClassTests.cs
@@ -0,0 +1,21 @@
+﻿namespace RubySharp.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class DefinedClassTests
+    {
+        [TestMethod]
+        public void CreateDefinedClass()
+        {
+            DefinedClass dclass = new DefinedClass("Dog");
+
+            Assert.AreEqual("Dog", dclass.Name);
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Language/DefinedClass.cs
@@ -0,0 +1,19 @@
+﻿namespace RubySharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DefinedClass
+    {
+        private string name;
+
+        public DefinedClass(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+    }
+}
