ca62ed484d14f827de263760ea84f0685194b996
ajlopez <webmaster@ajlopez.com>
2013-03-17 13:33:20 -0300

    DotExpression with tests

---
+++ Src/RubySharp.Core.Tests/Expressions/DotExpressionTests.cs
@@ -0,0 +1,32 @@
+﻿namespace RubySharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Expressions;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class DotExpressionTests
+    {
+        [TestMethod]
+        public void EvaluateObjectMethod()
+        {
+            Machine machine = new Machine();
+            machine.ExecuteText("class MyClass;def foo;3;end;end");
+
+            var dclass = (DefinedClass)machine.RootContext.GetValue("MyClass");
+
+            var myobj = dclass.CreateInstance();
+
+            DotExpression expression = new DotExpression(new ConstantExpression(myobj), "foo");
+
+            var result = expression.Evaluate(null);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Expressions/DotExpression.cs
@@ -0,0 +1,26 @@
+﻿namespace RubySharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Language;
+
+    public class DotExpression : IExpression
+    {
+        private IExpression expression;
+        private string name;
+
+        public DotExpression(IExpression expression, string name)
+        {
+            this.expression = expression;
+            this.name = name;
+        }
+
+        public object Evaluate(Context context)
+        {
+            var obj = (BaseObject)this.expression.Evaluate(context);
+            return obj.GetMethod(this.name).Apply(null);
+        }
+    }
+}
