cc28e33f8a1bd07338af4f55e398dfd988c2cfbb
ajlopez <webmaster@ajlopez.com>
2013-03-17 17:56:03 -0300

    Assign instance variable command

---
+++ Src/RubySharp.Core.Tests/Commands/AssignInstanceVarCommandTests.cs
@@ -0,0 +1,46 @@
+﻿namespace RubySharp.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Commands;
+    using RubySharp.Core.Expressions;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class AssignInstanceVarCommandTests
+    {
+        [TestMethod]
+        public void AssignValue()
+        {
+            AssignInstanceVarCommand cmd = new AssignInstanceVarCommand("one", new ConstantExpression(1));
+            BaseObject obj = new BaseObject(null);
+            Context context = new Context(null, obj, null);
+
+            var result = cmd.Execute(context);
+
+            Assert.AreEqual(1, result);
+            Assert.AreEqual(1, obj.GetValue("one"));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            AssignInstanceVarCommand cmd1 = new AssignInstanceVarCommand("a", new ConstantExpression(1));
+            AssignInstanceVarCommand cmd2 = new AssignInstanceVarCommand("a", new ConstantExpression(2));
+            AssignInstanceVarCommand cmd3 = new AssignInstanceVarCommand("b", new ConstantExpression(1));
+            AssignInstanceVarCommand cmd4 = new AssignInstanceVarCommand("a", new ConstantExpression(1));
+
+            Assert.IsTrue(cmd1.Equals(cmd4));
+            Assert.IsTrue(cmd4.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd4.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(cmd2));
+            Assert.IsFalse(cmd1.Equals(cmd3));
+            Assert.IsFalse(cmd1.Equals(123));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Commands/AssignInstanceVarCommand.cs
@@ -0,0 +1,53 @@
+﻿namespace RubySharp.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Expressions;
+
+    public class AssignInstanceVarCommand : ICommand
+    {
+        private static int hashtag = typeof(AssignInstanceVarCommand).GetHashCode();
+
+        private string name;
+        private IExpression expression;
+
+        public AssignInstanceVarCommand(string name, IExpression expression)
+        {
+            this.name = name;
+            this.expression = expression;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public object Execute(Context context)
+        {
+            object value = this.expression.Evaluate(context);
+            context.Self.SetValue(this.name, value);
+            return value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is AssignInstanceVarCommand)
+            {
+                var cmd = (AssignInstanceVarCommand)obj;
+
+                return this.Name.Equals(cmd.name) && this.Expression.Equals(cmd.Expression);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + this.Expression.GetHashCode() + hashtag;
+        }
+    }
+}
