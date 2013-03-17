9ebf7140a46b489599da85d0df789ca7cb4c6338
ajlopez <webmaster@ajlopez.com>
2013-03-17 08:08:33 -0300

    BaseObject first tests

---
+++ Src/RubySharp.Core.Tests/Language/BaseObjectTests.cs
@@ -0,0 +1,47 @@
+﻿namespace RubySharp.Core.Tests.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class BaseObjectTests
+    {
+        private DefinedClass @class;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.@class = new DefinedClass("Dog", null);
+        }
+
+        [TestMethod]
+        public void CreateObject()
+        {
+            BaseObject obj = new BaseObject(this.@class);
+
+            Assert.AreSame(this.@class, obj.Class);
+        }
+
+        [TestMethod]
+        public void GetUndefinedValue()
+        {
+            BaseObject obj = new BaseObject(this.@class);
+
+            Assert.IsNull(obj.GetValue("name"));
+        }
+
+        [TestMethod]
+        public void SetAndGetValue()
+        {
+            BaseObject obj = new BaseObject(this.@class);
+
+            obj.SetValue("name", "Nero");
+
+            Assert.AreEqual("Nero", obj.GetValue("name"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Language/BaseObject.cs
@@ -0,0 +1,33 @@
+﻿namespace RubySharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BaseObject
+    {
+        private DefinedClass @class;
+        private IDictionary<string, object> values = new Dictionary<string, object>();
+
+        public BaseObject(DefinedClass @class)
+        {
+            this.@class = @class;
+        }
+
+        public DefinedClass @Class { get { return this.@class; } }
+
+        public void SetValue(string name, object value)
+        {
+            this.values[name] = value;
+        }
+
+        public object GetValue(string name)
+        {
+            if (this.values.ContainsKey(name))
+                return this.values[name];
+
+            return null;
+        }
+    }
+}
