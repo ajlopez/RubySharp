c8b796f757c38797f91600945c92c58327fa46a0
ajlopez <webmaster@ajlopez.com>
2013-05-05 08:39:37 -0300

    For In command, first test

---
+++ Src/RubySharp.Core.Tests/Commands/ForInCommandTests.cs
@@ -0,0 +1,25 @@
+﻿namespace RubySharp.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Commands;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class ForInCommandTests
+    {
+        [TestMethod]
+        public void ExecuteForIn()
+        {
+            Context context = new Context();
+            context.SetValue("total", 0);
+            ForInCommand command = new ForInCommand("k", new ConstantExpression(new int[] { 1, 2, 3 }), new AssignCommand("total", new AddExpression(new NameExpression("total"), new NameExpression("k"))));
+            command.Execute(context);
+
+            Assert.AreEqual(6, context.GetValue("total"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Commands/ForInCommand.cs
@@ -0,0 +1,36 @@
+﻿namespace RubySharp.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Expressions;
+    using System.Collections;
+
+    public class ForInCommand : ICommand
+    {
+        private string name;
+        private IExpression expression;
+        private ICommand command;
+
+        public ForInCommand(string name,IExpression expression, ICommand command)
+        {
+            this.name = name;
+            this.expression = expression;
+            this.command = command;
+        }
+
+        public object Execute(Context context)
+        {
+            IEnumerable elements = (IEnumerable)this.expression.Evaluate(context);
+
+            foreach (var element in elements) 
+            {
+                context.SetValue(this.name, element);
+                this.command.Execute(context);
+            }
+
+            return null;
+        }
+    }
+}
