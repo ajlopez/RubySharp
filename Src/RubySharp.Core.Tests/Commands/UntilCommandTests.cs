750087db69085ce6ac1101f65d215e8db616d4d9
ajlopez <webmaster@ajlopez.com>
2013-10-13 13:27:56 -0300

    Until command

---
+++ Src/RubySharp.Core.Tests/Commands/UntilCommandTests.cs
@@ -0,0 +1,51 @@
+﻿namespace RubySharp.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Commands;
+    using RubySharp.Core.Compiler;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class UntilCommandTests
+    {
+        [TestMethod]
+        public void ExecuteSimpleUntilWhenConditionIsTrue()
+        {
+            Parser cmdparser = new Parser("a = a + 1");
+            IExpression body = cmdparser.ParseCommand();
+            Parser exprparser = new Parser("a >= 6");
+            IExpression expr = exprparser.ParseExpression();
+
+            Context context = new Context();
+            context.SetValue("a", 1);
+
+            UntilCommand cmd = new UntilCommand(expr, body);
+
+            Assert.IsNull(cmd.Evaluate(context));
+
+            Assert.AreEqual(6, context.GetValue("a"));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            UntilCommand cmd1 = new UntilCommand(new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(1)));
+            UntilCommand cmd2 = new UntilCommand(new ConstantExpression(2), new AssignCommand("one", new ConstantExpression(1)));
+            UntilCommand cmd3 = new UntilCommand(new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(2)));
+            UntilCommand cmd4 = new UntilCommand(new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(1)));
+
+            Assert.IsTrue(cmd1.Equals(cmd4));
+            Assert.IsTrue(cmd4.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd4.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(123));
+            Assert.IsFalse(cmd1.Equals(cmd2));
+            Assert.IsFalse(cmd1.Equals(cmd3));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Commands/UntilCommand.cs
@@ -0,0 +1,50 @@
+﻿namespace RubySharp.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Expressions;
+
+    public class UntilCommand : IExpression
+    {
+        private static int hashcode = typeof(UntilCommand).GetHashCode();
+
+        private IExpression condition;
+        private IExpression command;
+
+        public UntilCommand(IExpression condition, IExpression command)
+        {
+            this.condition = condition;
+            this.command = command;
+        }
+
+        public object Evaluate(Context context)
+        {
+            for (object value = this.condition.Evaluate(context); value == null || false.Equals(value); value = this.condition.Evaluate(context))
+                this.command.Evaluate(context);
+
+            return null;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is UntilCommand)
+            {
+                var cmd = (UntilCommand)obj;
+
+                return this.condition.Equals(cmd.condition) && this.command.Equals(cmd.command);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.condition.GetHashCode() + this.command.GetHashCode() + hashcode;
+        }
+    }
+}
