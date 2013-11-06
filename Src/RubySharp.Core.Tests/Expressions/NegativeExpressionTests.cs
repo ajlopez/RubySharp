b1ca8931fa08b7929d72db5b37ca29992763fb2e
ajlopez <webmaster@ajlopez.com>
2013-11-06 07:37:35 -0300

    NegativeExpression

---
+++ Src/RubySharp.Core.Tests/Expressions/NegativeExpressionTests.cs
@@ -0,0 +1,38 @@
+﻿namespace RubySharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class NegativeExpressionTests
+    {
+        [TestMethod]
+        public void NegativeIntegers()
+        {
+            NegativeExpression expr = new NegativeExpression(new ConstantExpression(1));
+
+            Assert.AreEqual(-1, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            NegativeExpression expr1 = new NegativeExpression(new ConstantExpression(1));
+            NegativeExpression expr2 = new NegativeExpression(new ConstantExpression(2));
+            NegativeExpression expr3 = new NegativeExpression(new ConstantExpression(1));
+
+            Assert.IsTrue(expr1.Equals(expr3));
+            Assert.IsTrue(expr3.Equals(expr1));
+            Assert.AreEqual(expr1.GetHashCode(), expr3.GetHashCode());
+
+            Assert.IsFalse(expr1.Equals(null));
+            Assert.IsFalse(expr1.Equals("foo"));
+            Assert.IsFalse(expr1.Equals(expr2));
+            Assert.IsFalse(expr2.Equals(expr1));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Expressions/NegativeExpression.cs
@@ -0,0 +1,48 @@
+﻿namespace RubySharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NegativeExpression : IExpression
+    {
+        private static int hashcode = typeof(NegativeExpression).GetHashCode();
+
+        private IExpression expression;
+        private IExpression right;
+
+        public NegativeExpression(IExpression expression)
+        {
+            this.expression = expression;
+        }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public object Evaluate(Context context)
+        {
+            var value = this.expression.Evaluate(context);
+            return -(int)value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is NegativeExpression)
+            {
+                var expr = (NegativeExpression)obj;
+
+                return this.expression.Equals(expr.expression);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Expression.GetHashCode() + hashcode;
+        }
+    }
+}
