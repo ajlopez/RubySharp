54b4661ee647cd6fc6824dae13c9a1dcd6e0fa8d
ajlopez <webmaster@ajlopez.com>
2013-11-12 19:05:43 -0300

    Subtract reals

--- Src/RubySharp.Core.Tests/Expressions/SubtractExpressionTests.cs
+++ Src/RubySharp.Core.Tests/Expressions/SubtractExpressionTests.cs
@@ -19,6 +19,32 @@
             Assert.AreEqual(1, expr.Evaluate(null));
         }
 
+
+        [TestMethod]
+        public void SubtractDoubleFromInteger()
+        {
+            SubtractExpression expr = new SubtractExpression(new ConstantExpression(1), new ConstantExpression(2.5));
+
+            Assert.AreEqual(1 - 2.5, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void SubtractIntegerFromDouble()
+        {
+            SubtractExpression expr = new SubtractExpression(new ConstantExpression(2.5), new ConstantExpression(1));
+
+            Assert.AreEqual(2.5 - 1, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void SubtractTwoDoubles()
+        {
+            SubtractExpression expr = new SubtractExpression(new ConstantExpression(2.5), new ConstantExpression(3.7));
+
+            Assert.AreEqual(2.5 - 3.7, expr.Evaluate(null));
+        }
+
+
         [TestMethod]
         public void Equals()
         {
