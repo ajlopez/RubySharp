97cd0e32fa83a11ab14717b6ffff3aadffe94c76
ajlopez <webmaster@ajlopez.com>
2013-11-17 20:36:02 -0300

    Additional tests for evaluation

---
+++ Src/RubySharp.Core.Tests/EvaluateTests.cs
@@ -0,0 +1,38 @@
+﻿namespace RubySharp.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Compiler;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class EvaluateTests
+    {
+        private Machine machine;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.machine = new Machine();
+        }
+
+        [TestMethod]
+        public void EvaluateSimpleArithmetic()
+        {
+            Assert.AreEqual(2, this.EvaluateExpression("1+1"));
+            Assert.AreEqual(1 / 2, this.EvaluateExpression("1/2"));
+            Assert.AreEqual(1 + 3 * 2, this.EvaluateExpression("1 + 3 * 2"));
+        }
+
+        private object EvaluateExpression(string text)
+        {
+            Parser parser = new Parser(text);
+            IExpression expression = parser.ParseExpression();
+            Assert.IsNull(parser.ParseExpression());
+            return expression.Evaluate(this.machine.RootContext);
+        }
+    }
+}
