655bc66a2e2911e5c7612a37a91e87a3d0cbc441
ajlopez <webmaster@ajlopez.com>
2013-11-17 20:41:18 -0300

    Additional tests for evaluating assignments

--- Src/RubySharp.Core.Tests/EvaluateTests.cs
+++ Src/RubySharp.Core.Tests/EvaluateTests.cs
@@ -27,12 +27,41 @@
             Assert.AreEqual(1 + 3 * 2, this.EvaluateExpression("1 + 3 * 2"));
         }
 
+        [TestMethod]
+        public void EvaluateSimpleAssignment()
+        {
+            Assert.AreEqual(2, this.Execute("a = 2"));
+            Assert.AreEqual(2, this.EvaluateExpression("a"));
+        }
+
+        [TestMethod]
+        public void EvaluateSimpleArray()
+        {
+            Assert.IsNotNull(this.Execute("a = [1,2,3]"));
+            var result = this.EvaluateExpression("a");
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IList<object>));
+
+            var list = (IList<object>)result;
+
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(2, list[1]);
+            Assert.AreEqual(3, list[2]);
+        }
+
         private object EvaluateExpression(string text)
         {
             Parser parser = new Parser(text);
             IExpression expression = parser.ParseExpression();
             Assert.IsNull(parser.ParseExpression());
             return expression.Evaluate(this.machine.RootContext);
+        }
+
+        private object Execute(string text)
+        {
+            return this.machine.ExecuteText(text);
         }
     }
 }
