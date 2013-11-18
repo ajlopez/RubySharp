2b99307e7ccaae57737a3c35bb57a717c27ba386
ajlopez <webmaster@ajlopez.com>
2013-11-18 18:55:35 -0300

    Indexed expression

---
+++ Src/RubySharp.Core.Tests/Expressions/IndexedExpressionTests.cs
@@ -0,0 +1,49 @@
+﻿namespace RubySharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Exceptions;
+    using RubySharp.Core.Expressions;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class IndexedExpressionTests
+    {
+        [TestMethod]
+        public void GetIndexedValue()
+        {
+            Machine machine = new Machine();
+            machine.ExecuteText("a = [1,2,3]");
+
+            IndexedExpression expression = new IndexedExpression(new NameExpression("a"), new ConstantExpression(1));
+
+            var result = expression.Evaluate(machine.RootContext);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            IndexedExpression expr1 = new IndexedExpression(new NameExpression("a"), new ConstantExpression(1));
+            IndexedExpression expr2 = new IndexedExpression(new NameExpression("a"), new ConstantExpression(2));
+            IndexedExpression expr3 = new IndexedExpression(new NameExpression("b"), new ConstantExpression(1));
+            IndexedExpression expr4 = new IndexedExpression(new NameExpression("a"), new ConstantExpression(1));
+
+            Assert.IsTrue(expr1.Equals(expr4));
+            Assert.IsTrue(expr4.Equals(expr1));
+            Assert.AreEqual(expr1.GetHashCode(), expr4.GetHashCode());
+
+            Assert.IsFalse(expr1.Equals(expr2));
+            Assert.IsFalse(expr2.Equals(expr1));
+            Assert.IsFalse(expr1.Equals(expr3));
+            Assert.IsFalse(expr3.Equals(expr1));
+            Assert.IsFalse(expr1.Equals(null));
+            Assert.IsFalse(expr1.Equals("foo"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Expressions/IndexedExpression.cs
@@ -0,0 +1,51 @@
+﻿namespace RubySharp.Core.Expressions
+{
+    using System.Collections.Generic;
+    using RubySharp.Core.Exceptions;
+    using RubySharp.Core.Language;
+
+    public class IndexedExpression : IExpression
+    {
+        private static int hashcode = typeof(IndexedExpression).GetHashCode();
+        private IExpression expression;
+        private IExpression indexexpression;
+
+        public IndexedExpression(IExpression expression, IExpression indexexpression)
+        {
+            this.expression = expression;
+            this.indexexpression = indexexpression;
+        }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public IExpression IndexExpression { get { return this.indexexpression; } }
+
+        public object Evaluate(Context context)
+        {
+            var list = (IList<object>)this.expression.Evaluate(context);
+            int index = (int)this.indexexpression.Evaluate(context);
+
+            return list[index];
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is IndexedExpression)
+            {
+                var expr = (IndexedExpression)obj;
+
+                return this.expression.Equals(expr.expression) && this.indexexpression.Equals(expr.indexexpression);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.expression.GetHashCode() + this.indexexpression.GetHashCode() + hashcode;
+        }
+    }
+}
