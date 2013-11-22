de7f850ee0c7f57140c77c26a99d7f4351765f32
ajlopez <webmaster@ajlopez.com>
2013-11-22 16:09:11 -0300

    Block first tests

---
+++ Src/RubySharp.Core.Tests/Language/BlockTests.cs
@@ -0,0 +1,50 @@
+﻿namespace RubySharp.Core.Tests.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Expressions;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class BlockTests
+    {
+        [TestMethod]
+        public void CreateAndEvaluateSimpleBlock()
+        {
+            Block block = new Block(null, new ConstantExpression(1));
+
+            Assert.AreEqual(1, block.Apply(null, null));
+        }
+
+        [TestMethod]
+        public void CreateAndEvaluateBlockWithFreeVariable()
+        {
+            Context context = new Context();
+            context.SetValue("a", 1);
+            Block block = new Block(null, new AddExpression(new NameExpression("a"), new ConstantExpression(1)));
+
+            Assert.AreEqual(2, block.Apply(context, null));
+        }
+
+        [TestMethod]
+        public void CreateAndEvaluateBlockWithArgument()
+        {
+            Block block = new Block(new string[] { "a", "b" }, new AddExpression(new NameExpression("a"), new NameExpression("b")));
+
+            Assert.AreEqual(3, block.Apply(null, new object[] { 1, 2 }));
+        }
+
+        [TestMethod]
+        public void CreateAndEvaluateBlockWithFreeVariableAndArgument()
+        {
+            Context context = new Context();
+            context.SetValue("a", 1);
+            Block block = new Block(new string[] { "b" }, new SubtractExpression(new NameExpression("a"), new NameExpression("b")));
+
+            Assert.AreEqual(-2, block.Apply(context, new object[] { 3 }));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Language/Block.cs
@@ -0,0 +1,36 @@
+﻿namespace RubySharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Expressions;
+
+    public class Block
+    {
+        private IList<string> argumentnames;
+        private IExpression expression;
+
+        public Block(IList<string> argumentnames, IExpression expression)
+        {
+            this.argumentnames = argumentnames;
+            this.expression = expression;
+        }
+
+        public object Apply(Context context, IList<object> arguments)
+        {
+            if (arguments == null || arguments.Count == 0)
+                return this.expression.Evaluate(context);
+
+            Context newcontext = new Context(context);
+
+            for (int k = 0; k < this.argumentnames.Count; k++)
+                if (k < arguments.Count)
+                    newcontext.SetValue(argumentnames[k], arguments[k]);
+                else
+                    newcontext.SetValue(argumentnames[k], null);
+
+            return this.expression.Evaluate(newcontext);
+        }
+    }
+}
