4c407518959cf543351e8206a826e24db7538051
ajlopez <webmaster@ajlopez.com>
2013-11-24 15:05:21 -0300

    DynamicHash ToString

---
+++ Src/RubySharp.Core.Tests/Language/DynamicHashTests.cs
@@ -0,0 +1,37 @@
+﻿namespace RubySharp.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class DynamicHashTests
+    {
+        [TestMethod]
+        public void EmptyHashToString()
+        {
+            DynamicHash hash = new DynamicHash();
+
+            Assert.AreEqual("{}", hash.ToString());
+        }
+
+        [TestMethod]
+        public void HashToString()
+        {
+            DynamicHash hash = new DynamicHash();
+
+            hash[new Symbol("one")] = 1;
+            hash[new Symbol("two")] = 2;
+
+            var result = hash.ToString();
+
+            Assert.IsTrue(result.StartsWith("{"));
+            Assert.IsTrue(result.EndsWith("}"));
+            Assert.IsTrue(result.Contains(":one=>1"));
+            Assert.IsTrue(result.Contains(":two=>2"));
+        }
+    }
+}
--- Src/RubySharp.Core/Language/DynamicHash.cs
+++ Src/RubySharp.Core/Language/DynamicHash.cs
@@ -7,5 +7,25 @@
 
     public class DynamicHash : Dictionary<object, object>
     {
+        public override string ToString()
+        {
+            var result = "{";
+
+            foreach (var key in this.Keys)
+            {
+                var value = this[key];
+
+                if (result.Length > 1)
+                    result += ", ";
+
+                result += key.ToString();
+                result += "=>";
+                result += value.ToString();
+            }
+
+            result += "}";
+
+            return result;
+        }
     }
 }
