e233b36db4f59e3ee2ac035fc770259fb1827a85
ajlopez <webmaster@ajlopez.com>
2013-11-24 17:23:25 -0300

    AssingIndexedExpression first tests

---
+++ Src/RubySharp.Core.Tests/Expressions/AssignIndexedExpressionTests.cs
@@ -0,0 +1,54 @@
+﻿namespace RubySharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class AssignIndexedExpressionTests
+    {
+        [TestMethod]
+        public void SimpleEvaluate()
+        {
+            var array = new int[] { 1, 2, 3 };
+            AssignIndexedExpression expr = new AssignIndexedExpression(new ConstantExpression(array), new ConstantExpression(1), new ConstantExpression(10));
+
+            var result = expr.Evaluate(null);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(10, result);
+
+            Assert.AreEqual(1, array[0]);
+            Assert.AreEqual(10, array[1]);
+            Assert.AreEqual(3, array[2]);
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            AssignIndexedExpression expr1 = new AssignIndexedExpression(new ConstantExpression(1), new ConstantExpression(2), new ConstantExpression(3));
+            AssignIndexedExpression expr2 = new AssignIndexedExpression(new ConstantExpression(10), new ConstantExpression(2), new ConstantExpression(3));
+            AssignIndexedExpression expr3 = new AssignIndexedExpression(new ConstantExpression(1), new ConstantExpression(20), new ConstantExpression(3));
+            AssignIndexedExpression expr4 = new AssignIndexedExpression(new ConstantExpression(1), new ConstantExpression(2), new ConstantExpression(30));
+            AssignIndexedExpression expr5 = new AssignIndexedExpression(new ConstantExpression(1), new ConstantExpression(2), new ConstantExpression(3));
+
+            Assert.IsFalse(expr1.Equals(null));
+            Assert.IsFalse(expr1.Equals(123));
+            Assert.IsFalse(expr1.Equals("foo"));
+
+            Assert.IsTrue(expr1.Equals(expr5));
+            Assert.IsTrue(expr5.Equals(expr1));
+            Assert.AreEqual(expr1.GetHashCode(), expr5.GetHashCode());
+
+            Assert.IsFalse(expr1.Equals(expr2));
+            Assert.IsFalse(expr2.Equals(expr1));
+            Assert.IsFalse(expr1.Equals(expr3));
+            Assert.IsFalse(expr3.Equals(expr1));
+            Assert.IsFalse(expr1.Equals(expr4));
+            Assert.IsFalse(expr4.Equals(expr1));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Expressions/AssignIndexedExpression.cs
@@ -0,0 +1,53 @@
+﻿namespace RubySharp.Core.Expressions
+{
+    using System.Collections.Generic;
+    using RubySharp.Core.Exceptions;
+    using RubySharp.Core.Language;
+    using System.Collections;
+
+    public class AssignIndexedExpression : IExpression
+    {
+        private static int hashcode = typeof(IndexedExpression).GetHashCode();
+        private IExpression leftexpression;
+        private IExpression indexexpression;
+        private IExpression expression;
+
+        public AssignIndexedExpression(IExpression leftexpression, IExpression indexexpression, IExpression expression)
+        {
+            this.leftexpression = leftexpression;
+            this.indexexpression = indexexpression;
+            this.expression = expression;
+        }
+
+        public object Evaluate(Context context)
+        {
+            var leftvalue = (IList)this.leftexpression.Evaluate(context);
+            int index = (int)this.indexexpression.Evaluate(context);
+            var newvalue = this.expression.Evaluate(context);
+
+            leftvalue[index] = newvalue;
+
+            return newvalue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is AssignIndexedExpression)
+            {
+                var expr = (AssignIndexedExpression)obj;
+
+                return this.leftexpression.Equals(expr.leftexpression) && this.expression.Equals(expr.expression) && this.indexexpression.Equals(expr.indexexpression);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.leftexpression.GetHashCode() + this.expression.GetHashCode() + this.indexexpression.GetHashCode() + hashcode;
+        }
+    }
+}
