bfe2cd2447aea8b5b6dc1993b2e3fe811b61d5b7
ajlopez <webmaster@ajlopez.com>
2013-11-24 18:11:34 -0300

    Range expression first tests

---
+++ Src/RubySharp.Core.Tests/Expressions/RangeExpressionTests.cs
@@ -0,0 +1,55 @@
+﻿namespace RubySharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Expressions;
+
+    [TestClass]
+    public class RangeExpressionTests
+    {
+        [TestMethod]
+        public void SimpleEvaluate()
+        {
+            RangeExpression expr = new RangeExpression(new ConstantExpression(1), new ConstantExpression(4));
+
+            var result = expr.Evaluate(null);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<int>));
+
+            var range = (IEnumerable<int>)result;
+
+            int total = 0;
+
+            foreach (int k in range)
+                total += k;
+
+            Assert.AreEqual(10, total);
+        }
+        [TestMethod]
+
+        public void Equals()
+        {
+            RangeExpression expr1 = new RangeExpression(new ConstantExpression(1), new ConstantExpression(2));
+            RangeExpression expr2 = new RangeExpression(new ConstantExpression(1), new ConstantExpression(3));
+            RangeExpression expr3 = new RangeExpression(new ConstantExpression(2), new ConstantExpression(2));
+            RangeExpression expr4 = new RangeExpression(new ConstantExpression(1), new ConstantExpression(2));
+
+            Assert.IsFalse(expr1.Equals(null));
+            Assert.IsFalse(expr1.Equals(123));
+            Assert.IsFalse(expr1.Equals("foo"));
+
+            Assert.IsTrue(expr1.Equals(expr4));
+            Assert.IsTrue(expr4.Equals(expr1));
+            Assert.AreEqual(expr1.GetHashCode(), expr4.GetHashCode());
+
+            Assert.IsFalse(expr1.Equals(expr2));
+            Assert.IsFalse(expr2.Equals(expr1));
+            Assert.IsFalse(expr1.Equals(expr3));
+            Assert.IsFalse(expr3.Equals(expr1));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Expressions/RangeExpression.cs
@@ -0,0 +1,44 @@
+﻿namespace RubySharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RubySharp.Core.Language;
+
+    public class RangeExpression
+    {
+        private static int hashcode = typeof(RangeExpression).GetHashCode();
+
+        private IExpression fromexpression;
+        private IExpression toexpression;
+
+        public RangeExpression(IExpression fromexpression, IExpression toexpression)
+        {
+            this.fromexpression = fromexpression;
+            this.toexpression = toexpression;
+        }
+
+        public object Evaluate(Context context)
+        {
+            int from = (int)this.fromexpression.Evaluate(context);
+            int to = (int)this.toexpression.Evaluate(context);
+            return new Range(from, to);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is RangeExpression))
+                return false;
+
+            var rexpr = (RangeExpression)obj;
+
+            return this.fromexpression.Equals(rexpr.fromexpression) && this.toexpression.Equals(rexpr.toexpression);
+        }
+
+        public override int GetHashCode()
+        {
+            return hashcode + this.fromexpression.GetHashCode() + 7 * this.toexpression.GetHashCode();
+        }
+    }
+}
