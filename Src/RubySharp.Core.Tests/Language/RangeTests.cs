9a4443b682b4fc0f19a614654df8a02f619ca959
ajlopez <webmaster@ajlopez.com>
2013-11-24 18:01:07 -0300

    Range first tests

---
+++ Src/RubySharp.Core.Tests/Language/RangeTests.cs
@@ -0,0 +1,57 @@
+﻿namespace RubySharp.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Language;
+
+    [TestClass]
+    public class RangeTests
+    {
+        [TestMethod]
+        public void EmptyRange()
+        {
+            int total = 0;
+            Range range = new Range(1, 0);
+
+            foreach (int k in range)
+                total += k;
+
+            Assert.AreEqual(0, total);
+        }
+
+        [TestMethod]
+        public void OneNumberRange()
+        {
+            int total = 0;
+            Range range = new Range(1, 1);
+
+            foreach (int k in range)
+                total += k;
+
+            Assert.AreEqual(1, total);
+        }
+
+        [TestMethod]
+        public void ThreeNumbersRange()
+        {
+            int total = 0;
+            Range range = new Range(1, 3);
+
+            foreach (int k in range)
+                total += k;
+
+            Assert.AreEqual(6, total);
+        }
+
+        [TestMethod]
+        public void RangeToString()
+        {
+            Range range = new Range(1, 3);
+
+            Assert.AreEqual("1..3", range.ToString());
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Language/Range.cs
@@ -0,0 +1,75 @@
+﻿namespace RubySharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+
+    public class Range : IEnumerable<int>
+    {
+        private int from;
+        private int to;
+
+        public Range(int from, int to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new RangeEnumerator(this.from, this.to);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new RangeEnumerator(this.from, this.to);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}..{1}", this.from, this.to);
+        }
+
+        private class RangeEnumerator : IEnumerator<int>
+        {
+            private int from;
+            private int to;
+            private int current;
+
+            public RangeEnumerator(int from, int to)
+            {
+                this.from = from;
+                this.to = to;
+                this.current = from - 1;
+            }
+
+            public object Current
+            {
+                get { return this.current; }
+            }
+
+            public bool MoveNext()
+            {
+                this.current++;
+
+                return this.current >= this.from && this.current <= this.to;
+            }
+
+            public void Reset()
+            {
+                this.current = this.from - 1;
+            }
+
+            int IEnumerator<int>.Current
+            {
+                get { return this.current; }
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
