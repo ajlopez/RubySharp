4ef48bfab814f6dcdc29369411a361a80908b43b
ajlopez <webmaster@ajlopez.com>
2013-11-25 08:42:35 -0300

    Static http server sample

--- Src/RubySharp.Console/Programs/httpserver.rb
+++ Src/RubySharp.Console/Programs/httpserver.rb
@@ -1,10 +1,12 @@
-﻿Array = System.Array
-Byte = System.Byte
+﻿Byte = System.Byte
 HttpListener = System.Net.HttpListener
 
-root = "c:/apache-tomcat-6.0.18/webapps/docs"
+FileStream = System.IO.FileStream
+FileMode = System.IO.FileMode
+Path = System.IO.Path
+File = System.IO.File
 
-bytes = Array.CreateInstance(Byte, 1024)
+Root = "c:/apache-tomcat-6.0.18/webapps/docs"
 
 listener = HttpListener.new
 listener.Prefixes.Add("http://*:8000/")
@@ -27,8 +29,31 @@ def process(context)
 	puts context.Request.Url.AbsolutePath
 	puts filename
 
-	filename = Path.Combine(root, filename)
+	filename = Path.Combine(Root, filename)
 
 	puts filename
+
+	if File.Exists(filename)
+		input = FileStream.new(filename, FileMode::Open)
+		bytes = System.Array.CreateInstance(Byte, 1024)
+		nbytes = input.Read(bytes, 0, bytes.Length)
+
+		while nbytes > 0
+			context.Response.OutputStream.Write(bytes, 0, nbytes)
+			nbytes = input.Read(bytes, 0, bytes.Length)
+		end
+
+		input.Close()
+		context.Response.OutputStream.Close()
+	else
+		context.Response.Abort()
+	end
 end
 
+listener.Start()
+
+while true
+    context = listener.GetContext()
+    puts "new request"
+    process(context)
+end        
