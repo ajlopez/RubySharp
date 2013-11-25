8a693ca9069835e9db1c4194a1bb3afc9b79b00e
ajlopez <webmaster@ajlopez.com>
2013-11-25 13:48:39 -0300

    MyLib class library

---
+++ Samples/MyLib/MyLib/MyClass.cs
@@ -0,0 +1,27 @@
+﻿namespace MyLib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MyClass
+    {
+        private string name;
+        private int age;
+
+        public MyClass()
+        {
+        }
+
+        public MyClass(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public int Age { get { return this.age; } }
+    }
+}
