d20793f03cdfe2c6df2239dce02f5eb1df0633af
ajlopez <webmaster@ajlopez.com>
2013-11-25 10:30:06 -0300

    Predicates first tests

---
+++ Src/RubySharp.Core.Tests/Language/PredicatesTests.cs
@@ -0,0 +1,50 @@
+﻿namespace RubySharp.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RubySharp.Core.Language;
+    using RubySharp.Core.Tests.Classes;
+
+    [TestClass]
+    public class PredicatesTests
+    {
+        [TestMethod]
+        public void IsFalse()
+        {
+            Assert.IsTrue(Predicates.IsFalse(null));
+            Assert.IsTrue(Predicates.IsFalse(false));
+            Assert.IsFalse(Predicates.IsFalse(string.Empty));
+            Assert.IsFalse(Predicates.IsFalse("foo"));
+            Assert.IsFalse(Predicates.IsFalse(123));
+            Assert.IsFalse(Predicates.IsFalse(0));
+            Assert.IsFalse(Predicates.IsFalse(0.0));
+            Assert.IsFalse(Predicates.IsFalse(typeof(Person)));
+        }
+
+        [TestMethod]
+        public void IsTrue()
+        {
+            Assert.IsFalse(Predicates.IsTrue(null));
+            Assert.IsFalse(Predicates.IsTrue(false));
+            Assert.IsTrue(Predicates.IsTrue(string.Empty));
+            Assert.IsTrue(Predicates.IsTrue("foo"));
+            Assert.IsTrue(Predicates.IsTrue(123));
+            Assert.IsTrue(Predicates.IsTrue(0));
+            Assert.IsTrue(Predicates.IsTrue(0.0));
+            Assert.IsTrue(Predicates.IsTrue(typeof(Person)));
+        }
+
+        [TestMethod]
+        public void IsConstantName()
+        {
+            Assert.IsFalse(Predicates.IsConstantName(null));
+            Assert.IsFalse(Predicates.IsConstantName(string.Empty));
+            Assert.IsFalse(Predicates.IsConstantName("foo"));
+            Assert.IsTrue(Predicates.IsConstantName("Foo"));
+            Assert.IsTrue(Predicates.IsConstantName("FOO"));
+        }
+    }
+}
---
+++ Src/RubySharp.Core/Language/Predicates.cs
@@ -0,0 +1,28 @@
+﻿namespace RubySharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class Predicates
+    {
+        public static bool IsFalse(object obj)
+        {
+            return obj == null || false.Equals(obj);
+        }
+
+        public static bool IsTrue(object obj)
+        {
+            return !IsFalse(obj);
+        }
+
+        public static bool IsConstantName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return false;
+
+            return char.IsUpper(name[0]);
+        }
+    }
+}
